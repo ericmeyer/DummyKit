990c9f8e66f4c6eaa1fc013ff65d678f5f7f10eb
Eric Meyer <emeyer@8thlight.com>
2020-06-19 09:49:27 -0500

    Initial Commit

---
+++ DummyKit/DummyKit.h
@@ -0,0 +1,19 @@
+//
+//  DummyKit.h
+//  DummyKit
+//
+//  Created by Eric Meyer on 6/19/20.
+//  Copyright © 2020 Eric Meyer. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DummyKit.
+FOUNDATION_EXPORT double DummyKitVersionNumber;
+
+//! Project version string for DummyKit.
+FOUNDATION_EXPORT const unsigned char DummyKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DummyKit/PublicHeader.h>
+
+
